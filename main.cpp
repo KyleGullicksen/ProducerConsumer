fd843886db0040d29fbc64eeb905d5d8f48185f9
Kyle Gullicksen <gulli003@cougars.csusm.edu>
2017-11-04 21:47:21 -0700

    Wait a minute, that can't be it, right?

--- main.cpp
+++ main.cpp
@@ -15,8 +15,64 @@
 
 using namespace std;
 
+#define DEFAULT_THREAD_ATTRIBUTES NULL
+
+void *producer(void * params);
+void *consumer(void * params);
+void initBuffer(vector<int> &buffer);
+
+vector<int> buffer;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int counter = 0;
+int producerIndex = 0;
+int consumerIndex = 0;
+bool go = false;
 
 int main()
 {
-    pthread_t thread1, thread2;
+    pthread_t producerThread, consumerThread;
+
+    //Create the producer and consumer threads
+    pthread_create(&producerThread, DEFAULT_THREAD_ATTRIBUTES, producer, NULL);
+    pthread_create(&consumerThread, DEFAULT_THREAD_ATTRIBUTES, consumer, NULL);
+
+    pthread_join(producerThread, NULL);
+    pthread_join(consumerThread, NULL);
+
+    exit(EXIT_SUCCESS);
 }
+
+
+void *producer(void * params)
+{
+   while(producerIndex < 100)
+   {
+       while(go);
+       pthread_mutex_lock(&lock);
+       buffer.push_back(counter);
+       counter++;
+       producerIndex++;
+       cout << "Producer: Counter value: " << counter << endl;
+       pthread_mutex_unlock(&lock);
+   }
+
+    go = true;
+
+}
+
+void *consumer(void * params)
+{
+    consumerIndex = 99;
+
+    while(consumerIndex > -1)
+    {
+        while(!go);
+        pthread_mutex_lock(&lock);
+        int erasedVal = buffer[consumerIndex];
+        buffer.erase(buffer.begin() + consumerIndex);
+        consumerIndex--;
+        cout << "Consumer: Erased value: " << erasedVal << endl;
+        pthread_mutex_unlock(&lock);
+    }
+}
+
